299277d6fff8242f13e2ff8fb1862b54a5a6e848
KAKUMANI LALITHA <113081900+kakumanilalitha21@users.noreply.github.com>
2022-10-06 18:51:48 +0530

    Update Average.java

--- Average.java
+++ Average.java
@@ -3,6 +3,7 @@ class Average {
  double nums[] = {10.1, 11.2, 12.3, 13.4, 14.5};
  double result = 0;
  int i;
+  //for loop
  for(i=0; i<5; i++)
  result = result + nums[i];
  System.out.println("Average is " + result / 5);
